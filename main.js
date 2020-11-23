9c9e63f0191cd4f4cb1b8e1f22ee7080eed121a8
vihaan2803 <74404558+vihaan2803@users.noreply.github.com>
2020-11-23 16:29:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function a() {
+    window.alert("The Books Are Those Who Make Us Intelligent, Sharp And Pure Mind Too.")
+}
+
+function b() {
+    window.alert("Reading Books Also Improve Our Language That We Want To Learn.")
+}
+
+function c() {
+    window.alert("When You Have A Doubt Go To Library.")
+}
+
+function d() {
+    window.alert("Because Of Books We Can Touch The Sky.")
+}
